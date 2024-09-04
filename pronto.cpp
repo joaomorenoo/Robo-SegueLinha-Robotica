70e2462be482981d2e798a9652e109ac0b2267fd
joaomorenoo <joaovitorm.123@gmail.com>
2024-09-03 21:43:03 -0300

    Pronto

--- pronto.cpp
+++ pronto.cpp
@@ -3,7 +3,9 @@
 #define emissorTrig 2
 #define receptorEcho 4
 
-NewPing sensorDistancia(emissorTrig, receptorEcho, 100); 
+#define buzzer 7 
+
+NewPing sensorDistancia(emissorTrig, receptorEcho, 100);
 
 #define sensorE 13
 #define sensorD 12
@@ -21,43 +23,54 @@ void setup() {
   pinMode(motorEN2, OUTPUT);
   pinMode(motorDN3, OUTPUT);
   pinMode(motorDN4, OUTPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
   bool EstadoD = digitalRead(sensorD);
   bool EstadoE = digitalRead(sensorE);
-  int Velocidade = 60;
+  int Velocidade = 40;
   int Distancia = sensorDistancia.ping_cm();
   
-  if(Distancia <= 6) {
-    digitalWrite(motorEN1, LOW); 
-    digitalWrite(motorEN2, LOW); 
-    digitalWrite(motorDN3, LOW); 
-    digitalWrite(motorDN4, LOW); 
-  } else {
-        if (!EstadoD && !EstadoE) {
-        analogWrite(motorEN1, Velocidade); 
-        analogWrite(motorEN2, 0); 
-        analogWrite(motorDN3, Velocidade); 
-        analogWrite(motorDN4, 0);
-        }
-        if (EstadoD && EstadoE) {
-        analogWrite(motorEN1, 0); 
-        analogWrite(motorEN2, 0); 
-        analogWrite(motorDN3, 0); 
-        analogWrite(motorDN4, 0); 
-        }
-        if (!EstadoD && EstadoE) {
-        analogWrite(motorEN1, 0); 
-        analogWrite(motorEN2, 0); 
-        analogWrite(motorDN3, Velocidade); 
-        analogWrite(motorDN4, 0); 
-        }
-        if (EstadoD && !EstadoE){
-        analogWrite(motorEN1, Velocidade); 
-        analogWrite(motorEN2, 0); 
-        analogWrite(motorDN3, 0); 
-        analogWrite(motorDN4, 0); 
-        }
+
+
+  if(Distancia >= 5 || Distancia == 0) {
+    if (!EstadoD && !EstadoE) {
+    analogWrite(motorEN1, 0); 
+    analogWrite(motorEN2, Velocidade); 
+    analogWrite(motorDN3, Velocidade); 
+    analogWrite(motorDN4, 0);
+    noTone(buzzer);
     }
+    if (EstadoD && EstadoE) {
+      analogWrite(motorEN1, 0); 
+      analogWrite(motorEN2, 0); 
+      analogWrite(motorDN3, 0); 
+      analogWrite(motorDN4, 0); 
+      noTone(buzzer);
+    }
+    if (!EstadoD && EstadoE) {
+      analogWrite(motorEN1, 0); 
+      analogWrite(motorEN2, 0); 
+      analogWrite(motorDN3, Velocidade); 
+      analogWrite(motorDN4, 0); 
+      noTone(buzzer);
+    }
+    if (EstadoD && !EstadoE){
+      analogWrite(motorEN1, 0); 
+      analogWrite(motorEN2, Velocidade); 
+      analogWrite(motorDN3, 0); 
+      analogWrite(motorDN4, 0);
+      noTone(buzzer); 
+    }
+  } else {
+    analogWrite(motorEN1, 0); 
+    analogWrite(motorEN2, 0); 
+    analogWrite(motorDN3, 0); 
+    analogWrite(motorDN4, 0); 
+    tone(buzzer, 392, 300);
+    //delay(100);
+    noTone(buzzer);
+  }  
 }
