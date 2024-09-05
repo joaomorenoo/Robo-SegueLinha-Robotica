b00462bf62379c9c9b87a3eebb1b89ab8f1d9f39
joaomorenoo <joaovitorm.123@gmail.com>
2024-09-04 21:46:58 -0300

    Update

--- pronto.cpp
+++ pronto.cpp
@@ -1,11 +1,7 @@
-#include <NewPing.h> 
-
 #define emissorTrig 2
 #define receptorEcho 4
 
-#define buzzer 7 
-
-NewPing sensorDistancia(emissorTrig, receptorEcho, 100);
+#define buzzer 7
 
 #define sensorE 13
 #define sensorD 12
@@ -14,6 +10,9 @@ NewPing sensorDistancia(emissorTrig, receptorEcho, 100);
 #define motorEN2 10
 #define motorDN3 6
 #define motorDN4 5
+
+long duracao;
+int distancia;
 
 void setup() {
   pinMode(sensorE, INPUT);
@@ -24,53 +23,67 @@ void setup() {
   pinMode(motorDN3, OUTPUT);
   pinMode(motorDN4, OUTPUT);
 
+  pinMode(emissorTrig, OUTPUT);
+  pinMode(receptorEcho, INPUT);
+  
+  pinMode(buzzer, OUTPUT);
+
   Serial.begin(9600);
 }
 
 void loop() {
   bool EstadoD = digitalRead(sensorD);
   bool EstadoE = digitalRead(sensorE);
-  int Velocidade = 40;
-  int Distancia = sensorDistancia.ping_cm();
-  
+  int Velocidade = 50;
+  calc_dist();
 
-
-  if(Distancia >= 5 || Distancia == 0) {
+  if(distancia >= 5 || distancia == 0) {
     if (!EstadoD && !EstadoE) {
     analogWrite(motorEN1, 0); 
     analogWrite(motorEN2, Velocidade); 
     analogWrite(motorDN3, Velocidade); 
     analogWrite(motorDN4, 0);
-    noTone(buzzer);
+    //musica();
     }
     if (EstadoD && EstadoE) {
       analogWrite(motorEN1, 0); 
       analogWrite(motorEN2, 0); 
       analogWrite(motorDN3, 0); 
       analogWrite(motorDN4, 0); 
-      noTone(buzzer);
+      //musica();
     }
     if (!EstadoD && EstadoE) {
       analogWrite(motorEN1, 0); 
       analogWrite(motorEN2, 0); 
       analogWrite(motorDN3, Velocidade); 
-      analogWrite(motorDN4, 0); 
-      noTone(buzzer);
+      analogWrite(motorDN4, 0);
     }
     if (EstadoD && !EstadoE){
       analogWrite(motorEN1, 0); 
       analogWrite(motorEN2, Velocidade); 
       analogWrite(motorDN3, 0); 
-      analogWrite(motorDN4, 0);
-      noTone(buzzer); 
+      analogWrite(motorDN4, 0); 
     }
   } else {
     analogWrite(motorEN1, 0); 
     analogWrite(motorEN2, 0); 
     analogWrite(motorDN3, 0); 
     analogWrite(motorDN4, 0); 
-    tone(buzzer, 392, 300);
-    //delay(100);
-    noTone(buzzer);
   }  
 }
+void calc_dist() {
+  digitalWrite(emissorTrig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(emissorTrig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(emissorTrig, LOW);
+  duracao = pulseIn(receptorEcho, HIGH);
+  distancia = duracao * 0.034 / 2;
+  Serial.println(distancia);
+}
+/*void musica(){
+  tone(buzzer,523); // Mi
+  delay (150);
+  tone(buzzer,587); // Fa
+  delay (150);
+} */ 
